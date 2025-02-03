bd993b18ac28e547f757ab3bee72956ec4b4d586
Lloyd Leano <leanolloyd@gmail.com>
2025-02-03 22:16:06 +0800

    [def__init__] If statements checking instances of float or string input.

--- Fraction.py
+++ Fraction.py
@@ -1,8 +1,23 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        #TODO
-        pass
+        
+        if isinstance(numerator, float):
+            self.numerator, self.denominator = 0, 1
+            return
+
+        if isinstance(numerator, str):
+            numerator = numerator.strip()
+            if "/" not in numerator:
+                self.numerator, self.denominator = 0, 1
+                return
+            
+            parts = numerator.split("/")
+            if len(parts) != 2 or not parts[0].lstrip("-").isdigit() or not parts[1].lstrip("-").isdigit():
+                self.numerator, self.denominator = 0, 1
+                return
+            
+            numerator, denominator = int(parts[0]), int(parts[1])
 
     def gcd(a, b):
         #TODO
